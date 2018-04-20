0a7b3f371daeff2d234db1a79a16703a6950deb9
James Tanner <tanner.jc@gmail.com>
2018-04-20 01:10:11 -0400

    Attempt to add snoopy

--- docker/cisco-ios/entrypoint.sh
+++ docker/cisco-ios/entrypoint.sh
@@ -1,17 +1,7 @@
 #!/bin/bash
 
-: ${SSH_USERNAME:=user}
-: ${SSH_USERPASS:=$(dd if=/dev/urandom bs=1 count=15 | base64)}
-
 __create_rundir() {
 	mkdir -p /var/run/sshd
-}
-
-__create_user() {
-# Create a user to SSH into as.
-useradd $SSH_USERNAME
-echo -e "$SSH_USERPASS\n$SSH_USERPASS" | (passwd --stdin $SSH_USERNAME)
-echo ssh user password: $SSH_USERPASS
 }
 
 __create_hostkeys() {
@@ -21,7 +11,5 @@ ssh-keygen -t rsa -f /etc/ssh/ssh_host_rsa_key -N ''
 # Call all functions
 __create_rundir
 __create_hostkeys
-#__create_user
 
 exec "$@"
-

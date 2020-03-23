8756cadaf0df1c3a7f5fb77ae0f47703e8dcf250
James Tanner <tanner.jc@gmail.com>
2020-03-23 19:25:38 -0400

    add setup and license

---
+++ setup.py
@@ -0,0 +1,23 @@
+import glob
+from setuptools import setup, find_packages
+
+
+with open('README.md') as f:
+    readme = f.read()
+
+
+with open('LICENSE.txt') as f:
+    license = f.read()
+
+setup(
+    name='ansible-tools',
+    version='0.1.0',
+    description='various tools for debuggers of ansible',
+    long_description=readme,
+    author='James Tanner',
+    author_email='tanner.jc@gmail.com',
+    url='https://github.com/jctanner/ansible-tools',
+    license=license,
+    packages=find_packages(),
+    scripts=[x for x in glob.glob('scripts/*') if not '__pycache__' in x]
+ )

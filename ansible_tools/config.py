d7d3720f99829dd1b4ca781ca7730b814f167032
James Tanner <tanner.jc@gmail.com>
2020-03-27 15:35:38 -0400

    add a lock for current workdir

--- ansible_tools/config.py
+++ ansible_tools/config.py
@@ -12,3 +12,14 @@ if not os.path.exists(ANSIBLE_TOOLS_CACHEDIR) and not os.access('/var/cache', os
 
 # ansible-workon uses this to create new dirs for working issues ...
 ANSIBLE_TOOLS_WORKDIR = os.path.expanduser(os.environ.get('ANSIBLE_TOOLS_WORKDIR', '~/workspace/issues'))
+
+# keep track of what the last workdir was ...
+ANSIBLE_TOOLS_WORKDIR_LOCKFILE = os.path.join(ANSIBLE_TOOLS_WORKDIR, '.current')
+
+# make it easy to get the dir ...
+ANSIBLE_TOOLS_WORKDIR_CURRENT = None
+if os.path.exists(ANSIBLE_TOOLS_WORKDIR_LOCKFILE):
+    with open(ANSIBLE_TOOLS_WORKDIR_LOCKFILE, 'r') as f:
+        ANSIBLE_TOOLS_WORKDIR_CURRENT = f.read().strip()
+
+
